7ebfde938f54996c56288d20d76fd52c4e38049e
karinagiuseppina <karina.giuseppina@gmail.com>
2021-08-27 19:56:35 +0000

    category and cards done

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,27 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 import "../../styles/home.scss";
+import { Link, useParams } from "react-router-dom";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container-fluid">
+		<div className="row">
+			<div className="col text-center">
+				<h1>Category</h1>
+			</div>
+		</div>
+		<div className="row">
+			<div className="col-3 m-auto text-center">
+				<div className="card">
+					<img src="https://via.placeholder.com/350x150" className="card-img-top" />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">Some quick.</p>
+						<Link to="#" className="btn btn-primary">
+							Go somewhere
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 );
